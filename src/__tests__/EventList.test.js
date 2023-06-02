d49e977a5384a616570272af3ed908e2778b192e
Gregpk55 <122652405+Gregpk55@users.noreply.github.com>
2023-06-02 16:41:23 -0700

    Events/ City Search Testing

---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import EventList from '../EventsList';
+import Event from '../Event'; // in src/__tests__/EventList.test.js
+import { mockData } from '../mock-data';
+describe('<EventList /> component', () => {
+  test('render correct number of events', () => {
+    const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
+  });
+});
